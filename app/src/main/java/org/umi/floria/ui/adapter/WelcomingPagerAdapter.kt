dbabd11dbdaa72d111f2ecda5327ac8561cea17f
rennoesche <rafi21089ti@student.nurulfikri.ac.id>
2023-11-25 23:29:56 +0700

    changed to viewpager2

--- app/src/main/java/org/umi/floria/ui/adapter/WelcomingPagerAdapter.kt
+++ app/src/main/java/org/umi/floria/ui/adapter/WelcomingPagerAdapter.kt
@@ -1,28 +1,24 @@
-package org.umi.floria.ui
+package org.umi.floria.ui.adapter
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import org.umi.floria.ui.fragment.Slide1Fragment
-import org.umi.floria.ui.fragment.Slide2Fragment
-import org.umi.floria.ui.fragment.Slide3Fragment
-import org.umi.floria.ui.fragment.Slide4Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.umi.floria.ui.fragment.*
 
 
-class WelcomingPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+class WelcomingPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
-    override fun getItem(position: Int): Fragment {
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> Slide1Fragment()
             1 -> Slide2Fragment()
             2 -> Slide3Fragment()
             3 -> Slide4Fragment()
-            // Tambahkan fragment lain sesuai kebutuhan
             else -> throw IllegalArgumentException("Invalid position: $position")
         }
-    }
-
-    override fun getCount(): Int {
-        return 4
     }
 }

13fa9048e1f1b1b9d5101638eee657c7e5003ea2
rennoesche <rafi21089ti@student.nurulfikri.ac.id>
2023-11-26 12:39:59 +0700

    add circle indicator

--- app/src/main/java/org/umi/floria/ui/WelcomingActivity.kt
+++ app/src/main/java/org/umi/floria/ui/WelcomingActivity.kt
@@ -4,28 +4,39 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import org.umi.floria.R
+import org.umi.floria.ui.adapter.WelcomingPagerAdapter
 
 class WelcomingActivity : AppCompatActivity() {
 
-    private lateinit var viewPager: ViewPager
+    private lateinit var viewPager: ViewPager2
+    private lateinit var indicatorLayout: LinearLayout
     private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sharedPreferences = getPreferences(Context.MODE_PRIVATE)
 
-        if (isFreshInstall()) {
+        if (sharedPreferences.getBoolean("FreshInstall", true)) {
             setContentView(R.layout.activity_welcoming)
             val btnStart: Button = findViewById(R.id.btnStart)
+            indicatorLayout = findViewById(R.id.indicatorLayout)
 
             viewPager = findViewById(R.id.viewPager)
 
-            val adapter = WelcomingPagerAdapter(supportFragmentManager)
+            val adapter = WelcomingPagerAdapter(this)
             viewPager.adapter = adapter
+
+            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    updateIndicator(position)
+                }
+            })
 
             btnStart.setOnClickListener {
                 setFreshInstall(false)
@@ -33,20 +44,27 @@ class WelcomingActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             }
-        }
-        else {
+        } else {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+    //    private fun isFreshInstall(): Boolean {
+//        return sharedPreferences.getBoolean("FreshInstall", true)
+//    }
 
+    private fun updateIndicator(position: Int) {
+        for (i in 0 until indicatorLayout.childCount) {
+            val circle = indicatorLayout.getChildAt(i) as View
+            circle.setBackgroundResource(if (i == position) R.drawable.sel_circle_bg else R.drawable.circle_bg)
+        }
     }
-    private fun isFreshInstall(): Boolean {
-        return sharedPreferences.getBoolean("FreshInstall", true)
-    }
+
     private fun setFreshInstall(value: Boolean) {
         val editor = sharedPreferences.edit()
         editor.putBoolean("FreshInstall", value)
         editor.apply()
     }
 }
+
